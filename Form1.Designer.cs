c2b22e22435c0f46575e446718f664d9af50a4da
Максим Кузьмин <cybermax@bk.ru>
2020-04-15 19:55:56 +1000

    Настройка главной формы.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,8 +35,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Fantasy King\'s Battle";
             this.ResumeLayout(false);
 
         }
