e0d9896583cc3c98b9b2bebe2f2783e74bc3a7e0
Максим Кузьмин <cybermax@bk.ru>
2020-04-16 15:27:04 +1000

    Добавлены классы умений, отрядов.

---
+++ Skill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_King_s_Battle
+{
+    internal sealed class Skill
+    {
+        public Skill()
+        {
+
+        }
+
+        internal string ID { get; }
+        internal string Name { get; }
+
+    }
+}
---
+++ Squad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_King_s_Battle.Resources.Config
+{
+    // Класс отряда
+    internal sealed class Squad
+    {
+        public Squad(TypeUnit typeUnit)
+        {
+            TypeUnit = typeUnit;
+        }
+
+        internal TypeUnit TypeUnit { get; }
+    }
+}
