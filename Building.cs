2786ab37c5758a8cb9159d42bede827d0c2bf505
Максим Кузьмин <cybermax@bk.ru>
2020-05-04 17:23:13 +1000

    Добавлен класс Construction. Класс Building наследник от Construction.

--- Building.cs
+++ Building.cs
@@ -11,16 +11,11 @@ namespace Fantasy_King_s_Battle
     internal enum TypeBuilding { Castle, Trade, Other };
 
     // Класс здания
-    internal sealed class Building
+    internal sealed class Building : Construction
     {
-        public Building(XmlNode n)
+        public Building(XmlNode n) : base (n)
         {
-            ID = n.SelectSingleNode("ID").InnerText;
-            Name = n.SelectSingleNode("Name").InnerText;
-            ImageIndex = Convert.ToInt32(n.SelectSingleNode("ImageIndex").InnerText);
             TypeBuilding = (TypeBuilding)Enum.Parse(typeof(TypeBuilding), n.SelectSingleNode("TypeBuilding").InnerText);
-            DefaultLevel = Convert.ToInt32(n.SelectSingleNode("DefaultLevel").InnerText);
-            MaxLevel = Convert.ToInt32(n.SelectSingleNode("MaxLevel").InnerText);
             Position = FormMain.Config.Buildings.Count;
 
             Debug.Assert(DefaultLevel >= 0);
@@ -40,34 +35,10 @@ namespace Fantasy_King_s_Battle
                 if (b.ImageIndex == ImageIndex)
                     throw new Exception("В конфигурации зданий повторяется ImageIndex = " + ImageIndex.ToString());
             }
-
-            // Загружаем информацию об уровнях
-            Levels = new Level[MaxLevel + 1];// Для упрощения работы с уровнями, добавляем 1, чтобы уровень был равен индексу в массиве
-
-            XmlNode nl = n.SelectSingleNode("Levels");
-            if (nl != null)
-            {
-                Level level;
-
-                foreach (XmlNode l in nl.SelectNodes("Level"))
-                {
-                    level = new Level(l);
-                    Debug.Assert(Levels[level.Pos] == null);
-                    Levels[level.Pos] = level;
-                }
-            }
         }
 
-        internal string ID { get; }
-        internal string Name { get; }
-        internal int ImageIndex { get; }
         internal int Position { get; }
         internal TypeBuilding TypeBuilding { get; }
-        internal int DefaultLevel { get; }
-        internal int MaxLevel { get; }
-
-        internal Level[] Levels;
-
         internal PanelBuilding Panel { get; set; }
     }
 }
---
+++ Construction.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Diagnostics;
+
+namespace Fantasy_King_s_Battle
+{
+    internal abstract class Construction
+    {
+        public Construction(XmlNode n)
+        {
+            ID = n.SelectSingleNode("ID").InnerText;
+            Name = n.SelectSingleNode("Name").InnerText;
+            ImageIndex = Convert.ToInt32(n.SelectSingleNode("ImageIndex").InnerText);
+            DefaultLevel = Convert.ToInt32(n.SelectSingleNode("DefaultLevel").InnerText);
+            MaxLevel = Convert.ToInt32(n.SelectSingleNode("MaxLevel").InnerText);
+
+            Debug.Assert(DefaultLevel >= 0);
+            Debug.Assert(MaxLevel > 0);
+            Debug.Assert(MaxLevel <= 3);
+            Debug.Assert(DefaultLevel <= MaxLevel);
+
+            // Загружаем информацию об уровнях
+            Levels = new Level[MaxLevel + 1];// Для упрощения работы с уровнями, добавляем 1, чтобы уровень был равен индексу в массиве
+
+            XmlNode nl = n.SelectSingleNode("Levels");
+            if (nl != null)
+            {
+                Level level;
+
+                foreach (XmlNode l in nl.SelectNodes("Level"))
+                {
+                    level = new Level(l);
+                    Debug.Assert(Levels[level.Pos] == null);
+                    Levels[level.Pos] = level;
+                }
+            }
+        }
+
+        internal string ID { get; }
+        internal string Name { get; }
+        internal int ImageIndex { get; }
+        internal int DefaultLevel { get; }
+        internal int MaxLevel { get; }
+
+        internal Level[] Levels;
+    }
+}
