f76bf561f63a0cdeb806e44ca316595312cabee0
Максим Кузьмин <cybermax@bk.ru>
2020-05-27 22:28:47 +1000

    Добавлены Assert.

--- TypeLobby.cs
+++ TypeLobby.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Diagnostics;
 
 namespace Fantasy_King_s_Battle
 {
@@ -16,6 +17,14 @@ namespace Fantasy_King_s_Battle
             QuantityPlayers = Convert.ToInt32(n.SelectSingleNode("QuantityPlayers").InnerText);
             DurabilityCastle = Convert.ToInt32(n.SelectSingleNode("DurabilityCastle").InnerText);
             Gold = Convert.ToInt32(n.SelectSingleNode("Gold").InnerText);
+
+            Debug.Assert(QuantityPlayers >= 2);
+            Debug.Assert(QuantityPlayers >= 8);
+            Debug.Assert(QuantityPlayers % 2 == 0);
+            Debug.Assert(DurabilityCastle > 0);
+            Debug.Assert(DurabilityCastle <= 1000);
+            Debug.Assert(Gold >= 0);
+            Debug.Assert(Gold <= 100000);
         }
 
         internal string Name { get; }
