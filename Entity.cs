562982328bc21200611d9acfbe5e1edaa2db84ac
Максим Кузьмин <cybermax@bk.ru>
2020-05-31 00:42:55 +1000

    Класс Entity - базовый для всех сущностей игры.

---
+++ Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_King_s_Battle
+{
+    // Класс сущности. Общий для предметов, абилок, героев и т.д.
+    internal class Entity
+    {
+
+    }
+
+
+}
