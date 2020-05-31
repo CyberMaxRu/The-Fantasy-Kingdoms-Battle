8f5ad4979b7b476bb4f6862599e09a96c616f309
Максим Кузьмин <cybermax@bk.ru>
2020-05-31 15:48:13 +1000

    Класс PanelBaseInfo. Класс PanelBuildingInfo потомок от PanelBaseInfo.

---
+++ PanelBaseInfo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Fantasy_King_s_Battle
+{
+    // Базовый класс панели информации
+    internal abstract class PanelBaseInfo : BasePanel
+    {
+        private readonly PictureBox pbIcon;
+
+        public PanelBaseInfo(int width, int height) : base(true)
+        {
+            Width = width;
+            Height = height;
+            DoubleBuffered = true;
+
+            pbIcon = new PictureBox()
+            {
+                Parent = this,
+                Location = new Point(Config.GRID_SIZE, Config.GRID_SIZE),
+                Size = GetImageList().ImageSize,
+                BackColor = Color.Transparent
+            };
+        }
+
+        // Используемые потомками методы
+        protected int TopForControls() => GuiUtils.NextTop(pbIcon);
+        protected int LeftForControls() => pbIcon.Left;
+
+        // Переопределяемые потомками методы
+        protected abstract ImageList GetImageList();
+        protected abstract int GetImageIndex();
+
+        // Общие для всех панелей методы
+        internal virtual void ShowData()
+        {
+            pbIcon.Image = GetImageList().Images[GetImageIndex()];
+        }
+    }
+}
--- PanelBuildingInfo.cs
+++ PanelBuildingInfo.cs
@@ -9,11 +9,10 @@ using System.Drawing;
 namespace Fantasy_King_s_Battle
 {
     // Класс подробной информации о строении
-    internal sealed class PanelBuildingInfo : BasePanel
+    internal sealed class PanelBuildingInfo : PanelBaseInfo
     {
         private enum Page { Products, Warehouse, Inhabitants };
 
-        private PictureBox pbBuilding;
         private PlayerBuilding building;
         private PictureBox pbProducts;
         private PictureBox pbWarehouse;
@@ -24,25 +23,13 @@ namespace Fantasy_King_s_Battle
 //        private List<PanelEntity> panelProducts = new List<PanelEntity>();
         private Page activePage;
 
-        public PanelBuildingInfo(int width, int height) : base(true)
+        public PanelBuildingInfo(int width, int height) : base(width, height)
         {
-            Width = width;
-            Height = height;
-            DoubleBuffered = true;
-
-            pbBuilding = new PictureBox()
-            {
-                Parent = this,
-                Location = new Point(Config.GRID_SIZE, Config.GRID_SIZE),
-                Size = Program.formMain.ilBuildings.ImageSize,
-                BackColor = Color.Transparent
-            };
-
             pbProducts = new PictureBox()
             {
                 Parent = this,
-                Left = pbBuilding.Left,
-                Top = GuiUtils.NextTop(pbBuilding),
+                Left = LeftForControls(),
+                Top = TopForControls(),
                 Size = GuiUtils.SizeButtonWithImage(Program.formMain.ilGui),
                 BackgroundImage = Program.formMain.bmpForBackground,
                 Image = Program.formMain.ilGui.Images[FormMain.GUI_PRODUCTS]
@@ -53,7 +40,7 @@ namespace Fantasy_King_s_Battle
             {
                 Parent = this,
                 Left = GuiUtils.NextLeft(pbProducts),
-                Top = GuiUtils.NextTop(pbBuilding),
+                Top = pbProducts.Top,
                 Size = GuiUtils.SizeButtonWithImage(Program.formMain.ilGui),
                 BackgroundImage = Program.formMain.bmpForBackground,
                 Image = Program.formMain.ilGui.Images[FormMain.GUI_INVENTORY]
@@ -64,7 +51,7 @@ namespace Fantasy_King_s_Battle
             {
                 Parent = this,
                 Left = GuiUtils.NextLeft(pbWarehouse),
-                Top = GuiUtils.NextTop(pbBuilding),
+                Top = pbWarehouse.Top,
                 Size = GuiUtils.SizeButtonWithImage(Program.formMain.ilGui),
                 BackgroundImage = Program.formMain.bmpForBackground,
                 Image = Program.formMain.ilGui.Images[FormMain.GUI_INHABITANTS]
@@ -146,9 +133,9 @@ namespace Fantasy_King_s_Battle
             }
         }
 
-        internal void ShowData()
+        internal override void ShowData()
         {
-            pbBuilding.Image = Program.formMain.ilBuildings.Images[GuiUtils.GetImageIndexWithGray(Program.formMain.ilBuildings, Building.Building.ImageIndex, Building.Level > 0)];
+            base.ShowData();
 
             pbWarehouse.Visible = building.Building.TrainedHero != null;
             pbInhabitants.Visible = building.Building.TrainedHero != null;
@@ -183,6 +170,16 @@ namespace Fantasy_King_s_Battle
             }
 
             Show();    
+        }
+
+        protected override ImageList GetImageList()
+        {
+            return Program.formMain.ilBuildings;
+        }
+
+        protected override int GetImageIndex()
+        {
+            return GuiUtils.GetImageIndexWithGray(Program.formMain.ilBuildings, Building.Building.ImageIndex, Building.Level > 0);
         }
     }
 }
