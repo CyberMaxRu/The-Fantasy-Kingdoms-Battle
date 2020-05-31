a06d1ebb64b371287d7dce17e545e0cf1cf24e14
Максим Кузьмин <cybermax@bk.ru>
2020-05-31 15:11:56 +1000

    Класс Ability. Файл конфигурации способностей.

---
+++ Ability.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Fantasy_King_s_Battle
+{
+    // Класс способности
+    internal sealed class Ability : Entity
+    {
+    }
+}
