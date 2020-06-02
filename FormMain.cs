28c560077c6cceba5a0c13c4823231f9d0597151
Максим Кузьмин <cybermax@bk.ru>
2020-06-02 21:34:49 +1000

    Увеличен размер шрифта цены с 11 до 12.

--- FormMain.cs
+++ FormMain.cs
@@ -36,7 +36,7 @@ namespace Fantasy_King_s_Battle
 
         internal readonly Font fontLevel = new Font("Microsoft Sans Serif", 10, FontStyle.Bold);
         internal readonly Font fontQuantity = new Font("Courier New", 14, FontStyle.Bold);
-        internal readonly Font fontCost = new Font("Arial", 11, FontStyle.Bold);
+        internal readonly Font fontCost = new Font("Arial", 12, FontStyle.Bold);
         internal readonly Color ColorLevel = Color.Yellow;
         internal readonly Color ColorCost = Color.White;
         internal readonly Color ColorQuantity = Color.Yellow;
