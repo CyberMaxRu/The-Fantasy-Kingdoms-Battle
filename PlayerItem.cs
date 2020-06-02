5d0b68bfe90d7f2389f3c7deda0dba36879b978d
Максим Кузьмин <cybermax@bk.ru>
2020-06-02 22:14:24 +1000

    PlayerItem реализует ICell.

--- PlayerItem.cs
+++ PlayerItem.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Fantasy_King_s_Battle
 {
     // Класс предмета у игрока (находящегося на герое или на складе)
-    internal sealed class PlayerItem
+    internal sealed class PlayerItem : ICell
     {
         public PlayerItem(Item i, int quantity)
         {
@@ -21,5 +22,15 @@ namespace Fantasy_King_s_Battle
 
         internal Item Item { get; }
         internal int Quantity { get; set; }// Количество предметов
+
+        // Реализация интерфейса
+        ImageList ICell.ImageList() => Program.formMain.ilItems;
+        int ICell.ImageIndex() => Item.ImageIndex;
+        int ICell.Level() => 0;
+        int ICell.Quantity() => Quantity;
+        void ICell.PrepareHint()
+        {
+
+        }
     }
 }
