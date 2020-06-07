e4f20ef3791f65c8a9c2850808ede1ce5037c9c3
Максим Кузьмин <cybermax@bk.ru>
2020-06-07 17:02:42 +1000

    Класс Ammunition для оружия и брони.

---
+++ Resources/Config/Ammunition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_King_s_Battle.Resources.Config
+{
+    // Класс амуниции (оружие, броня и т.д.)
+    internal class Ammunition
+    {
+    }
+}
