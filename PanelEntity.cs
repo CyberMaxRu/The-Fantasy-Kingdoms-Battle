546a95a92af3e80f24285607a4637eff35fa9cfe
Максим Кузьмин <cybermax@bk.ru>
2020-12-07 22:34:28 +1000

    Переработка рисования в ячейке.

--- PanelEntity.cs
+++ PanelEntity.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 
 namespace Fantasy_King_s_Battle
 {
@@ -25,6 +26,11 @@ namespace Fantasy_King_s_Battle
     internal sealed class PanelEntity : Label
     {
         private ICell cell;
+        private bool lastEmptyCell;
+        private int lastImageIndex;
+        private bool lastNormal;
+        private bool lastSelected;
+        private Bitmap bmpImage;
 
         public PanelEntity()
         {
@@ -36,6 +42,8 @@ namespace Fantasy_King_s_Battle
             Font = FormMain.Config.FontQuantity;
 
             ShowHint = true;
+
+            bmpImage = new Bitmap(Size.Width, Size.Height);
         }
 
         internal bool ShowHint { get; set; }
@@ -44,7 +52,7 @@ namespace Fantasy_King_s_Battle
         {
             base.OnMouseEnter(e);
 
-            if ((cell != null) && (ShowHint == true))
+            if ((cell != null) && ShowHint)
             {
                 Program.formMain.formHint.Clear();
                 cell.PrepareHint();
@@ -63,22 +71,13 @@ namespace Fantasy_King_s_Battle
         {
             base.OnPaintBackground(e);
 
-            if (cell != null)
-            {
-                //Debug.Assert(cell.Panel == this);
-                
-                e.Graphics.DrawImageUnscaled(GuiUtils.GetImageFromImageList(cell.ImageList(), cell.ImageIndex(), cell.NormalImage()), FormMain.Config.ShiftForBorder);
-                if (Program.formMain.SelectedPanelEntity == this)
-                {
-                    e.Graphics.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
-                    e.Graphics.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
-                    e.Graphics.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
-                    e.Graphics.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
-                }
-                e.Graphics.DrawImageUnscaled(Program.formMain.bmpBorderForIcon, 0, 0);
-            }
-            else
-                e.Graphics.DrawImage(Program.formMain.bmpEmptyEntity, new Rectangle(1, 0, Program.formMain.bmpBorderForIcon.Width - 2, Program.formMain.bmpBorderForIcon.Height - 2));
+            //if ((lastEmptyCell != (cell == null)) || ((cell != null) && ((lastImageIndex != cell.ImageIndex()) || (lastNormal != cell.NormalImage()) || (lastSelected != (Program.formMain.SelectedPanelEntity == this)))))
+                DrawCell();
+
+            e.Graphics.CompositingMode = CompositingMode.SourceOver;
+            e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+            e.Graphics.CompositingQuality = CompositingQuality.HighSpeed;
+            e.Graphics.DrawImageUnscaled(bmpImage, e.ClipRectangle);
         }
 
         protected override void Dispose(bool disposing)
@@ -111,7 +110,42 @@ namespace Fantasy_King_s_Battle
 
             Text = (cell != null) && (cell.Value() > 0) ? c.Value().ToString() : "";
 
+            DrawCell();
             Invalidate();
+        }
+
+        private void DrawCell()
+        {
+            Graphics g = Graphics.FromImage(bmpImage);
+
+            if (cell != null)
+            {
+                //Debug.Assert(cell.Panel == this);
+
+                g.DrawImageUnscaled(GuiUtils.GetImageFromImageList(cell.ImageList(), cell.ImageIndex(), cell.NormalImage()), FormMain.Config.ShiftForBorder);
+
+                if (Program.formMain.SelectedPanelEntity == this)
+                {
+                    g.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
+                    g.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
+                    g.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
+                    g.DrawImage(Program.formMain.ilMenuCellFilters.Images[2], new Rectangle(0, 0, Width - 1, Height - 1));
+                }
+
+                g.DrawImageUnscaled(Program.formMain.bmpBorderForIcon, 0, 0);
+            }
+            else
+                g.DrawImage(Program.formMain.bmpEmptyEntity, new Rectangle(1, 0, Program.formMain.bmpBorderForIcon.Width - 2, Program.formMain.bmpBorderForIcon.Height - 2));
+
+            g.Dispose();
+
+            lastEmptyCell = cell == null;
+            if (!lastEmptyCell)
+            {
+                lastImageIndex = cell.ImageIndex();
+                lastNormal = cell.NormalImage();
+                lastSelected = Program.formMain.SelectedPanelEntity == this;
+            }
         }
     }
 }
