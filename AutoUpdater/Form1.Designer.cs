0460be93b5121b9cb253d90eaa8ef3237655a676
Максим Кузьмин <cybermax@bk.ru>
2020-12-08 22:10:46 +1000

    Реализация обновлятора.

--- AutoUpdater/Form1.Designer.cs
+++ AutoUpdater/Form1.Designer.cs
@@ -30,36 +30,59 @@ namespace AutoUpdater
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.labelActualVersion = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(8, 32);
+            this.button1.Location = new System.Drawing.Point(8, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(256, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Проверить на наличие новой версии";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // labelVersion
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 8);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Текущая версия";
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(8, 8);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(91, 13);
+            this.labelVersion.TabIndex = 1;
+            this.labelVersion.Text = "Текущая версия";
+            // 
+            // labelActualVersion
+            // 
+            this.labelActualVersion.AutoSize = true;
+            this.labelActualVersion.Location = new System.Drawing.Point(8, 56);
+            this.labelActualVersion.Name = "labelActualVersion";
+            this.labelActualVersion.Size = new System.Drawing.Size(308, 13);
+            this.labelActualVersion.TabIndex = 2;
+            this.labelActualVersion.Text = "Нажмите кнопку для проверки необходимости обновления";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(8, 72);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 216);
+            this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(459, 302);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelActualVersion);
+            this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "AutoUpdater The Fantasy Kingdoms Battle";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Updater The Fantasy Kingdoms Battle";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -68,7 +91,9 @@ namespace AutoUpdater
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Label labelActualVersion;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- AutoUpdater/Form1.cs
+++ AutoUpdater/Form1.cs
@@ -7,14 +7,241 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+using System.IO.Compression;
 
 namespace AutoUpdater
 {
     public partial class Form1 : Form
     {
+        private string dirResources;
+        private Version currentVersion;
+        private Version actualVersion;
+        private string curVersionRelease;
+        private string URLDrive;
+        private string UIDVersion;
+        private string UIDArchive;
+        private bool needUpdate = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // Настройка переменной с папкой ресурсов
+            dirResources = Environment.CurrentDirectory;
+
+            if (dirResources.Contains("Debug"))
+                dirResources = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 21);
+            else if (dirResources.Contains("Release"))
+                dirResources = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 32);
+            else
+                dirResources += "\\";
+
+            dirResources += "Resources\\";
+
+            // Открываем файл main.xml
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(dirResources + "Main.xml");
+            try
+            {
+                currentVersion = GetVersionFromXml(xmlDoc.SelectSingleNode("Main/Version"));
+
+                URLDrive = xmlDoc.SelectSingleNode("Main/AutoUpdate/URLDrive").InnerText;
+                if (URLDrive.Length == 0)
+                    throw new Exception("Не указан путь к drive.google.com.");
+                UIDVersion = xmlDoc.SelectSingleNode("Main/AutoUpdate/UIDVersion").InnerText;
+                if (UIDVersion.Length == 0)
+                    throw new Exception("Не указан UID файла с версиями.");
+                UIDArchive = xmlDoc.SelectSingleNode("Main/AutoUpdate/UIDArchive").InnerText;
+                if (UIDArchive.Length == 0)
+                    throw new Exception("Не указан UID файла с обновлением.");
+            }
+            catch (Exception exc)
+            {
+                ShowError(exc.Message);
+                Close();
+            }
+
+            if ((currentVersion.Major == 0) && (currentVersion.Minor == 0) && (currentVersion.Build == 0))
+            {
+                ShowError("Не найден текущий номер версии.");
+                Close();
+            }
+
+            labelVersion.Text = "Текущая версия: " + currentVersion + ".";
+        }
+
+        private void ShowError(string text)
+        {
+            MessageBox.Show(text, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (needUpdate)
+                Update();
+            else
+                CheckUpdate();
+        }
+
+        private void CheckUpdate()
+        {
+            // Скачиваем файл с версиями
+            string filenameVersion = Environment.CurrentDirectory + @"\ActualVersion.xml";
+            SetState("Скачиваем файл с версиями...");
+
+            if (DownloadFile(UIDVersion, filenameVersion))
+            {
+                // Смотрим, какая там последняя версия
+                try
+                {
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(filenameVersion);
+
+                    actualVersion = GetVersionFromXml(xmlDoc.SelectSingleNode("Versions/ActualVersion"));
+                    if (actualVersion > currentVersion)
+                    {
+                        SetState("Найдена новая версия: " + actualVersion.ToString());
+                        button1.Text = "Обновить игру";
+                        needUpdate = true;
+                        ShowChanges(xmlDoc);
+                    }
+                    else if (actualVersion == currentVersion)
+                    {
+                        labelActualVersion.Text = "У вас актуальная версия игры";
+                    }
+                    else
+                    {
+                        labelActualVersion.Text = "У вас более новая версия игры, чем должна была быть!";
+                    }
+                }
+                catch (Exception exc)
+                {
+                    ShowError(exc.Message);
+                }
+            }
+            else
+            {
+                labelActualVersion.Text = "Произошла ошибка при скачивании файла с версиями.";
+            }
+        }
+
+        private void ShowChanges(XmlDocument d)
+        {
+            textBox1.Clear();
+
+            XmlNode nc = d.SelectSingleNode("Versions");
+
+            string changes = "";
+            string line;
+            Version v;
+            foreach(XmlNode n in nc.SelectNodes("Version"))
+            {
+                v = GetVersionFromXml(n);
+                line = v.ToString() + " от " + n.SelectSingleNode("DateBuild").InnerText + Environment.NewLine;
+
+                foreach (XmlNode ld in n.SelectNodes("Description/Line"))
+                {
+                    line += ld.InnerText + Environment.NewLine;
+                }
+
+                if (v > currentVersion)
+                    changes = "---Изменения в установленной версии игры:" + Environment.NewLine + changes;
+                changes = line + Environment.NewLine + changes;
+            }
+
+            textBox1.Text = changes;
+        }
+
+        private void Update()
+        {
+            string pathUpdate = Environment.CurrentDirectory + @"\update";
+            // Скачиваем архив
+            SetState("Скачиваем архив с обновлением...");
+
+            string filenameZip = Environment.CurrentDirectory + @"\update.zip";
+            if (DownloadFile(UIDArchive, filenameZip))
+            {
+                // Удаляем папку с обновлениями
+                if (System.IO.Directory.Exists(pathUpdate))
+                { 
+                    SetState("Удаляем папку с обновлением...");
+                    System.IO.Directory.Delete(pathUpdate, true);
+                }
+
+                // Распаковка архива
+                SetState("Распаковываем архив...");
+                ZipFile.ExtractToDirectory(filenameZip, pathUpdate);
+
+                // Замена файлов
+                SetState("Заменяем файлы...");
+                string newName;
+                foreach (string file in System.IO.Directory.EnumerateFiles(pathUpdate))
+                {
+                    newName = Environment.CurrentDirectory + @"\" + System.IO.Path.GetFileName(file);
+                    if (System.IO.File.Exists(newName))
+                        System.IO.File.Delete(newName);
+                    System.IO.File.Move(file, newName);
+                }
+
+                string newdir;
+                foreach (string dir in System.IO.Directory.EnumerateDirectories(pathUpdate))
+                {
+                    newdir = Environment.CurrentDirectory + dir.Substring(Environment.CurrentDirectory.Length + 7);
+                    System.IO.Directory.Move(dir, newdir);
+                }
+
+                // Удаляем архив
+                SetState("Удаляем папку и архив с обновлением...");
+                if (System.IO.Directory.Exists(pathUpdate))
+                    System.IO.Directory.Delete(pathUpdate, true);
+                System.IO.File.Delete(filenameZip);
+
+                // Обновление завершено
+                SetState("Обновление завершено...");
+            }
+            else
+            {
+                labelActualVersion.Text = "Произошла ошибка при скачивании файла с обновлением.";
+            }
+        }
+
+        private static Version GetVersionFromXml(XmlNode n)
+        {
+            return new Version(Convert.ToByte(n.SelectSingleNode("Major").InnerText),
+                Convert.ToByte(n.SelectSingleNode("Minor").InnerText),
+                Convert.ToByte(n.SelectSingleNode("Build").InnerText));
+        }
+
+        private bool DownloadFile(string uid, string filename)
+        {
+            WebClient client = new WebClient();
+            try
+            {
+                client.DownloadFile("https://" + URLDrive + "&id=" + uid, filename);
+                return true;
+            }
+            catch (Exception exc)
+            {
+                ShowError(exc.Message);
+                return false;
+            }
+            finally
+            {
+                client.Dispose();
+            }
+        }
+
+        private void SetState(string text)
+        {
+            labelActualVersion.Text = text;
+            labelActualVersion.Refresh();
         }
     }
 }
