0b95490ee696187bd4ee1557aa910fbfda62adca
Максим Кузьмин <cybermax@bk.ru>
2020-12-12 19:05:53 +1000

    Корректировка формы updater'а.

--- AutoUpdater/Form1.Designer.cs
+++ AutoUpdater/Form1.Designer.cs
@@ -80,6 +80,8 @@ namespace AutoUpdater
             this.Controls.Add(this.labelActualVersion);
             this.Controls.Add(this.labelVersion);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Updater The Fantasy Kingdoms Battle";
