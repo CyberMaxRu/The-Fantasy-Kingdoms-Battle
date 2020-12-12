2da6eb5f97fcd466d3387d47777771dd3aed9647
Максим Кузьмин <cybermax@bk.ru>
2020-12-12 20:27:09 +1000

    Корректировка имени сборки Updater.

--- Updater/Properties/AssemblyInfo.cs
+++ Updater/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("AutoUpdater")]
+[assembly: AssemblyTitle("Updater")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AutoUpdater")]
+[assembly: AssemblyProduct("Updater")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
