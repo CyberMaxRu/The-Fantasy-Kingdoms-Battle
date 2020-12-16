24a8f15081829d9926cb2d361f4e7e0f65df3c5e
Максим Кузьмин <cybermax@bk.ru>
2020-12-16 21:32:29 +1000

    Версия 0.2.5.

--- FormMain.cs
+++ FormMain.cs
@@ -19,8 +19,8 @@ namespace Fantasy_King_s_Battle
     public partial class FormMain : Form
     {
         private const string NAME_PROJECT = "The Fantasy Kingdoms Battle";
-        internal const string VERSION = "0.2.4";
-        internal const string DATE_VERSION = "14.12.2020";
+        internal const string VERSION = "0.2.5";
+        internal const string DATE_VERSION = "16.12.2020";
         private const string VERSION_POSTFIX = "в разработке";
         internal readonly string dirCurrent;
         internal readonly string dirResources;
