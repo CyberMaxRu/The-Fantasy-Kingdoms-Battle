8682cc6b3478102ef84cb238d35afdece1b56b91
Максим Кузьмин <cybermax@bk.ru>
2021-01-05 15:49:27 +1000

    Общий для всех объектов класс Object. Общий для всех юнитов класс Unit.

---
+++ Object.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml;
+using System.Windows.Forms;
+
+namespace Fantasy_King_s_Battle
+{
+    // Базовый класс для всех объектов - зданий, логов, юнитов
+    internal abstract class Object
+    {
+        public Object(XmlNode n)
+        {
+            ID = n.SelectSingleNode("ID").InnerText;
+            Name = n.SelectSingleNode("Name").InnerText;
+            Description = Utils.AdaptDescription(n.SelectSingleNode("Description").InnerText);
+            ImageIndex = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("ImageIndex"));
+
+            Debug.Assert(ID.Length > 0);
+            Debug.Assert(Name.Length > 0);
+            Debug.Assert(Description.Length > 0);
+            Debug.Assert(ImageIndex >= 0);
+        }
+
+        internal string ID { get; }
+        internal string Name { get; }
+        internal string Description { get; set; }
+        internal int ImageIndex { get; }
+    }
+}
---
+++ Unit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml;
+
+namespace Fantasy_King_s_Battle
+{
+    // Базовый класс для всех юнитов
+    internal class Unit : Object
+    {
+        public Unit(XmlNode n) : base (n)
+        {
+
+        }
+    }
+}
