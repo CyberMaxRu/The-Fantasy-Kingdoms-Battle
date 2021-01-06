d69751634fc235c187a1ddc3534c358697b071dc
Максим Кузьмин <cybermax@bk.ru>
2021-01-06 16:11:45 +1000

    Настройка монстров и награды для уровня в логове. Загрузка монстров уровня.

--- Lair.cs
+++ Lair.cs
@@ -8,6 +8,89 @@ using System.Diagnostics;
 
 namespace Fantasy_King_s_Battle
 {
+    // Класс монстров уровня логова
+    internal sealed class MonsterLevelLair
+    {
+        private string idMonster;
+
+        public MonsterLevelLair(XmlNode n)
+        {
+            idMonster = n.SelectSingleNode("ID").InnerText;
+            StartQuantity = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("StartQuantity"));
+            MaxQuantity = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("MaxQuantity"));
+            Level = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("Level"));
+            DaysRespawn = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("DaysRespawn"));
+            QuantityRespawn = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("QuantityRespawn"));
+
+            Debug.Assert(idMonster.Length > 0);
+            Debug.Assert(StartQuantity >= 0);
+            Debug.Assert(MaxQuantity > 0);
+            Debug.Assert(StartQuantity <= MaxQuantity);
+            Debug.Assert(Level > 0);
+            Debug.Assert(DaysRespawn >= 0);
+            Debug.Assert(DaysRespawn <= 25);
+            Debug.Assert(QuantityRespawn >= 0);
+            //Debug.Assert(QuantityRespawn <= 49);
+        }
+
+        internal Monster Monster { get; private set; }
+        internal int StartQuantity { get; }
+        internal int MaxQuantity { get; }
+        internal int Level { get; }
+        internal int DaysRespawn { get; }
+        internal int QuantityRespawn { get; }
+        internal List<LairMonster> Monsters { get; } = new List<LairMonster>();
+
+        internal void SetDeferredLink()
+        {
+            Monster = FormMain.Config.FindMonster(idMonster);
+            idMonster = null;
+            Debug.Assert(Level <= Monster.MaxLevel);
+
+            // Создаем монстров
+            /*LairMonster m;
+            for (int i = 0; i < StartQuantity; i++)
+            {
+                m = new LairMonster(Monster, Level);
+                Monsters.Add(m);
+            }*/
+        }
+    }
+
+    // Класс награды за зачистку уровень логова
+    internal sealed class RewardLevelLair
+    {
+        public RewardLevelLair(XmlNode n)
+        {
+            Gold = XmlUtils.GetParamFromXmlInteger(n.SelectSingleNode("Gold"));
+
+            Debug.Assert(Gold >= 0);
+            Debug.Assert(Gold <= 50_000);
+        }
+
+        internal int Gold { get; }
+    }
+
+    // Класс уровня логова
+    internal sealed class LevelLair
+    {
+        public LevelLair(XmlNode n)
+        {
+            // Информация о монстрах
+            MonsterLevelLair mll;
+
+            foreach (XmlNode l in n.SelectNodes("Monster"))
+            {
+                mll = new MonsterLevelLair(l);
+                MonsterLevelLairs.Add(mll);
+            }
+            // Информация о награде
+        }
+
+        internal List<MonsterLevelLair> MonsterLevelLairs { get; } = new List<MonsterLevelLair>();
+        internal List<RewardLevelLair> RewardLevelLairs { get; } = new List<RewardLevelLair>();
+    }
+
     // Класс логова монстров
     internal sealed class Lair
     {
@@ -34,6 +117,21 @@ namespace Fantasy_King_s_Battle
                 if (l.ImageIndex == ImageIndex)
                     throw new Exception("В конфигурации логов повторяется ImageIndex = " + ImageIndex.ToString());
             }
+
+            // Загружаем информацию об уровнях
+            XmlNode nl = n.SelectSingleNode("Levels");
+            if (nl != null)
+            {
+                LevelLair level;
+
+                foreach (XmlNode l in nl.SelectNodes("Level"))
+                {
+                    level = new LevelLair(l);
+                    LevelLairs.Add(level);
+                }
+            }
+            //else
+            //    throw new Exception("В конфигурации логова у " + ID + " нет информации об уровнях. ");
         }
 
         internal string ID { get; }
@@ -41,6 +139,7 @@ namespace Fantasy_King_s_Battle
         internal string Description { get; }
         internal int ImageIndex { get; }
         internal int Line { get; }
+        internal List<LevelLair> LevelLairs { get; } = new List<LevelLair>();
         internal PanelLair Panel { get; set; }
 
     }
---
+++ LairMonster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_King_s_Battle
+{
+    // Класс монстра в логове
+    internal sealed class LairMonster
+    {
+        public LairMonster(Monster m, int level)
+        {            
+            Monster = m;
+
+        }
+
+        internal Monster Monster { get; }
+    }
+}
