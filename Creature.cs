1a12378cddbff6c578619e74032fa17425132066
Максим Кузьмин <cybermax@bk.ru>
2021-01-06 17:59:15 +1000

    Класс Creature - абстрактный класс существа.

---
+++ Creature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_King_s_Battle
+{
+    // Базовый класс существа
+    internal abstract class Creature
+    {
+    }
+}
