28a8028552479743eb7c12ed18b2f8a0df694694
Максим Кузьмин <cybermax@bk.ru>
2021-01-10 02:02:37 +1000

    ControlContainer поддерживает другие ControlContainer на себе.

--- ControlContainer.cs
+++ ControlContainer.cs
@@ -9,15 +9,21 @@ using System.Drawing;
 
 namespace Fantasy_King_s_Battle
 {
-    internal sealed class ControlContainer
+    internal class ControlContainer
     {
-        private Dictionary<Control, Point> Controls = new Dictionary<Control, Point>();
+        private Control parent;
         private int left;
         private int top;
+        private int width;
+        private int height;
 
-        public ControlContainer(Control parent)
+        public ControlContainer()
         {
-            Parent = parent;
+        }
+
+        public ControlContainer(Control aParent)
+        {
+            parent = aParent;
 
             //DoubleBuffered = true;
             //BackColor = Color.Transparent;
@@ -27,15 +33,41 @@ namespace Fantasy_King_s_Battle
             //BackgroundImage = GuiUtils.MakeBackground(Size);
         }
 
-        internal Control Parent { get; }
-        internal int Left { get { return left; } set { left = value; ArrangeControls(); } }
-        internal int Top { get { return top; } set { top = value; ArrangeControls(); } }
+        internal Control Parent
+        {
+            get => parent;
+            set
+            {
+                parent = value;
 
-        private void ArrangeControls()
+                foreach (KeyValuePair<Control, Point> p in Controls)
+                {
+                    p.Key.Parent = parent;
+                }
+
+                foreach (KeyValuePair<ControlContainer, Point> p in Containers)
+                {
+                    p.Key.Parent = parent;
+                }
+            }
+        }
+        internal int Left { get { return left; } set { left = value; ArrangeControlsAndContainers(); } }
+        internal int Top { get { return top; } set { top = value; ArrangeControlsAndContainers(); } }
+        internal int Width { get { return width; } set { width = value; } }
+        internal int Height { get { return height; } set { height = value; } }
+        internal Dictionary<Control, Point> Controls = new Dictionary<Control, Point>();
+        internal Dictionary<ControlContainer, Point> Containers = new Dictionary<ControlContainer, Point>();
+
+        private void ArrangeControlsAndContainers()
         {
             foreach (KeyValuePair<Control, Point> p in Controls)
             {
                 ArrangeControl(p);
+            }
+
+            foreach (KeyValuePair<ControlContainer, Point> p in Containers)
+            {
+                ArrangeContainer(p);
             }
         }
 
@@ -45,13 +77,36 @@ namespace Fantasy_King_s_Battle
             p.Key.Top = Top + p.Value.Y;
         }
 
+        private void ArrangeContainer(KeyValuePair<ControlContainer, Point> p)
+        {
+            p.Key.Left = Left + p.Value.X;
+            p.Key.Top = Top + p.Value.Y;
+        }
+
         internal void AddControl(Control c, Point p)
         {
+            Debug.Assert(c != null);
+            Debug.Assert(c.Parent == null);
+
             Controls.Add(c, p);
 
             c.Visible = false;
-            c.Parent = Parent;
+            if (Parent != null)
+                c.Parent = Parent;
+
             ArrangeControl(Controls.Last());
+        }
+
+        internal void AddContainer(ControlContainer cc, Point p)
+        {
+            Debug.Assert(cc != this);
+
+            Containers.Add(cc, p);
+
+            if ((cc.Parent == null) && (parent != null))
+                cc.Parent = parent;
+            cc.SetVisible(false);
+            ArrangeContainer(Containers.Last());
         }
 
         internal void SetVisible(bool visible)
@@ -59,6 +114,11 @@ namespace Fantasy_King_s_Battle
             foreach (KeyValuePair<Control, Point> c in Controls)
             {
                 c.Key.Visible = visible;
+            }
+
+            foreach (KeyValuePair<ControlContainer, Point> cc in Containers)
+            {
+                cc.Key.SetVisible(visible);
             }
         }
 
@@ -72,6 +132,19 @@ namespace Fantasy_King_s_Battle
             }
 
             return maxSize;
+        }
+
+        internal void Repaint()
+        {
+            foreach (KeyValuePair<Control, Point> p in Controls)
+            {
+                p.Key.Invalidate();
+            }
+
+            foreach (KeyValuePair<ControlContainer, Point> p in Containers)
+            {
+                p.Key.Repaint();
+            }
         }
     }
 }
