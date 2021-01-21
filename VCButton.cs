103ae20027581f054ec5145cb6189d7eebb3fde8
Максим Кузьмин <cybermax@bk.ru>
2021-01-21 21:48:44 +1000

    Рисование бордюра у VCButton через свойство VisualControl.

--- VCButton.cs
+++ VCButton.cs
@@ -11,10 +11,10 @@ namespace Fantasy_King_s_Battle
     // Визуальный контрол - кнопка c иконкой
     internal class VCButton : VCImage
     {
-        private readonly Pen penBorder = new Pen(FormMain.Config.CommonBorder);
-
         public VCButton(VisualControl parent, int shiftX, int shiftY, ImageList imageList, int imageIndex) : base(parent, shiftX, shiftY, imageList, imageIndex)
         {
+            ShowBorder = true;
+            BorderColor = FormMain.Config.CommonBorder;
         }
 
         protected override void ValidateSize()
@@ -24,10 +24,9 @@ namespace Fantasy_King_s_Battle
         }
 
         protected override void DrawImage(Graphics g)
-        { 
-            if (ImageIndex == -1)
-                g.Clear(Color.Transparent);
-            g.DrawRectangle(penBorder, Left, Top, Width - 1, Height - 1);
+        {
+            //base.DrawImage(g);
+
             if (ImageIndex >= 0)
                 g.DrawImageUnscaled(GuiUtils.GetImageFromImageList(ImageList, ImageIndex, true), Left + 2, Top + 2);
         }
