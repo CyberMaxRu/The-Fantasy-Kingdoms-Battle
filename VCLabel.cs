02ab402d4fdc161dff7796d3062c573cfb1e7dae
Максим Кузьмин <cybermax@bk.ru>
2021-01-22 20:40:16 +1000

    Доработка VCLabel.

--- VCLabel.cs
+++ VCLabel.cs
@@ -8,43 +8,49 @@ using System.Drawing;
 
 namespace Fantasy_King_s_Battle
 {
+    // Класс метки
     internal sealed class VCLabel : VisualControl
     {
-        private Label label;
+        private Brush brush;
+        private Color color;
+        private RectangleF rectText;
 
         public VCLabel(VisualControl parent, int shiftX, int shiftY, Font font, Color foreColor, int height, string text) : base(parent, shiftX, shiftY)
         {
             Height = height;
+            Text = text;
+            Font = font;
+            Color = foreColor;
 
-            label = new Label();
-            label.AutoSize = false;
-            label.TextAlign = ContentAlignment.MiddleCenter;
-            label.ForeColor = foreColor;
-            label.BackColor = Color.Transparent;
-            label.Text = text;
-            label.Font = font;
-            label.Height = height;
+            StringFormat = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Near
+            };
         }
 
-        internal override void Draw(Graphics g)
+        internal string Text { get; set; }
+        internal Font Font { get; set; }
+        internal Color Color
         {
-            //Bitmap bmp = new Bitmap(label.Width, label.Height);
-            //bmp.MakeTransparent();
-            //g.cle
-
-            StringFormat sf = new StringFormat();
-            sf.Alignment = StringAlignment.Center;
-            sf.LineAlignment = StringAlignment.Near;
-            g.DrawString(label.Text, label.Font, FormMain.Config.brushControl, new RectangleF(label.Left, label.Top, label.Width, label.Height), sf);
-//            label.DrawToBitmap(b, new Rectangle(label.Left, label.Top, label.Width, label.Height));
+            get => Color;
+            set { if (color != value) { color = value; brush?.Dispose(); brush = new SolidBrush(color); } }
         }
+        internal StringFormat StringFormat { get; set; }
 
         internal override void ArrangeControls()
         {
-            label.Left = Left;
-            label.Top = Top;
-            label.Width = Width;
-            //label.Height = Height;
+            base.ArrangeControls();
+
+            // Рисуем выше на два пиксела и увеличиваем высоту, так как у текст сверху пустота, а снизу происходит обрезка,
+            // хотя по высоте все вмещается
+            rectText = new RectangleF(Left, Top - 2, Width, Height + 2);
+        }
+        internal override void Draw(Graphics g)
+        {
+            base.Draw(g);
+
+            g.DrawString(Text, Font, brush, rectText, StringFormat);
         }
     }
 }
