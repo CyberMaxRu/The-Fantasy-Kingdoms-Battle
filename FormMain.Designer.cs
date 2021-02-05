a33fd587408469f61ad91b0693c396f9ec3aad40
Максим Кузьмин <cybermax@bk.ru>
2021-02-05 22:55:48 +1000

    Размер главного окна.

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -46,7 +46,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(979, 877);
+            this.ClientSize = new System.Drawing.Size(789, 438);
             this.Controls.Add(this.axWindowsMediaPlayer1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
