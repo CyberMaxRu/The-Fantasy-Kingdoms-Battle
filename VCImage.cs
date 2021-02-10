cf2aedc9890395f22cd5df18665d531097ab4bb5
Максим Кузьмин <cybermax@bk.ru>
2021-02-10 20:18:00 +1000

    R Чистка using.

--- VCImage.cs
+++ VCImage.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Windows.Forms;
-using System.Diagnostics;
 
 namespace Fantasy_Kingdoms_Battle
 {
