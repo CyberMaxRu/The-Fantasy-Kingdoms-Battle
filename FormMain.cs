7c6119653a2285a1006280eafbc0ac300a15452d
Максим Кузьмин <cybermax@bk.ru>
2021-02-27 14:30:24 +1000

    Иконка отмены флага.

--- FormMain.cs
+++ FormMain.cs
@@ -100,6 +100,7 @@ namespace Fantasy_Kingdoms_Battle
         internal const int GUI_SCROLL = 20;
         internal const int GUI_SETTINGS = 21;
         internal const int GUI_FLAG_SCOUT = 22;
+        internal const int GUI_FLAG_CANCEL = 23;
 
         internal const int GUI_PARAMETER_STRENGTH = 6;
         internal const int GUI_PARAMETER_DEXTERITY = 7;
