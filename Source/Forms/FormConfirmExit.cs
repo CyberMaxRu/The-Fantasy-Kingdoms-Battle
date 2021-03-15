2239d3347ed7d790e699186523766cd89e3d322b
Максим Кузьмин <cybermax@bk.ru>
2021-03-15 22:20:02 +1000

    Добавлен класс VCTextM2. В FormConfirmExit сделан вывод текста подтверждения о выходе через VCTextM2.

--- Source/Forms/FormConfirmExit.cs
+++ Source/Forms/FormConfirmExit.cs
@@ -11,10 +11,16 @@ namespace Fantasy_Kingdoms_Battle
     {
         private readonly VCButton bntOk;
         private readonly VCButton bntCancel;
+        private readonly VCTextM2 textConfirm;
 
         public FormConfirmExit() : base()
         {
             windowCaption.Caption = "Выход из программы";
+
+            textConfirm = new VCTextM2(ClientControl, 0, 0, Program.formMain.fontParagraph, Color.White, ClientControl.Width);
+            textConfirm.Text = "Выход приведет к потере текущей игры."
+                + " Здесь добавлен длинный текст, чтобы он не поместился на одной строке, а занял минимум три."
+                + " Продолжить?";
 
             bntOk = new VCButton(ClientControl, 0, 100, "Да");
             bntOk.Width = 160;
---
+++ Source/VisualControls/VCTextM2.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Визуальный контрол - текст
+
+    internal class VCTextM2 : VisualControl
+    {
+        private Bitmap bmpPreparedText;
+        private string preparedText;
+
+        public VCTextM2(VisualControl parent, int shiftX, int shiftY, M2Font font, Color foreColor, int width) : base(parent, shiftX, shiftY)
+        {
+            Width = width;
+            Font = font;
+            Color = foreColor;
+
+            StringFormat = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Near
+            };
+        }
+
+        internal string Text { get; set; }
+        internal M2Font Font { get; set; }
+        internal Color Color { get; set; }
+        internal StringFormat StringFormat { get; set; }
+
+        internal override void Draw(Graphics g)
+        {
+            if (Text.Length > 0)
+            {
+                if (preparedText != Text)
+                {
+                    bmpPreparedText?.Dispose();
+                    bmpPreparedText = Font.GetBitmap(Text, Color);
+                }
+            }
+
+            base.Draw(g);
+
+            if (Text.Length > 0)
+            {
+                int x;
+                int y;
+                switch (StringFormat.Alignment)
+                {
+                    case StringAlignment.Near:
+                        x = Left;
+                        break;
+                    case StringAlignment.Center:
+                        x = Left + ((Width - bmpPreparedText.Width) / 2);
+                        break;
+                    default:
+                        x = Left + Width - bmpPreparedText.Width;
+                        break;
+                }
+
+                switch (StringFormat.LineAlignment)
+                {
+                    case StringAlignment.Near:
+                        y = Top;
+                        break;
+                    case StringAlignment.Center:
+                        y = Top + ((Height - bmpPreparedText.Height) / 2);
+                        break;
+                    default:
+                        y = Top + Height - bmpPreparedText.Height;
+                        break;
+                }
+
+                g.DrawImageUnscaled(bmpPreparedText, x, y);
+            }
+        }
+    }
+}
