d52f824ead7e049a8e07f16a28aea4e7ec9a8e1a
Максим Кузьмин <cybermax@bk.ru>
2021-03-15 17:07:54 +1000

    Добавлен класс VCBitmapBand - он работает с картинкой, которая можент растягиваться. Класс VCSeparator - потомок от VCBitmapBand.

---
+++ Source/VisualControls/VCBitmapBand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Визуальный контрол - растягиваемая картинка
+    // Состоит из левой части, середины (повторяемой) и правой части
+
+    internal abstract class VCBitmapBand : VisualControl
+    {
+        private Bitmap bmp;
+
+        public VCBitmapBand(VisualControl parent, int shiftX, int shiftY) : base(parent, shiftX, shiftY)
+        {
+            Height = GetBitmap().Height;
+        }
+
+        internal override void Draw(Graphics g)
+        {
+            Bitmap bmpBand = GetBitmap();
+            Debug.Assert(Width >= bmpBand.Width);
+
+            if ((bmp == null) || (bmp.Width != Width))
+            {
+                bmp?.Dispose();
+                int widthCap = WidthCap();
+                int widthBody = bmpBand.Width - widthCap - widthCap;
+                Debug.Assert(widthBody > 0);
+
+                bmp = new Bitmap(Width, Program.formMain.bmpSeparator.Height);
+                Graphics gb = Graphics.FromImage(bmp);
+
+                gb.DrawImage(bmpBand, 0, 0, new Rectangle(0, 0, widthCap, widthCap), GraphicsUnit.Pixel);
+                gb.DrawImage(bmpBand, bmp.Width - widthCap, 0, new Rectangle(widthBody + widthBody, 0, widthCap, widthCap), GraphicsUnit.Pixel);
+
+                Rectangle rectBand = new Rectangle(widthCap, 0, widthBody, widthCap);
+                for (int i = widthCap; i < bmp.Width - widthCap; i++)
+                {
+                    gb.DrawImage(bmpBand, i, 0, rectBand, GraphicsUnit.Pixel);
+                }
+
+                gb.Dispose();
+            }
+
+            base.Draw(g);
+
+            g.DrawImageUnscaled(bmp, Left, Top);
+        }
+
+        protected abstract int WidthCap();
+        protected abstract Bitmap GetBitmap();
+    }
+}
--- Source/VisualControls/VCSeparator.cs
+++ Source/VisualControls/VCSeparator.cs
@@ -9,41 +9,13 @@ using System.Diagnostics;
 namespace Fantasy_Kingdoms_Battle
 {
     // Визуальный контрол - разделитель
-    internal sealed class VCSeparator : VisualControl
+    internal sealed class VCSeparator : VCBitmapBand
     {
-        private Bitmap bmp;
-
         public VCSeparator(VisualControl parent, int shiftX, int shiftY) : base(parent, shiftX, shiftY)
         {
-            Height = Program.formMain.bmpSeparator.Height;
         }
 
-        internal override void Draw(Graphics g)
-        {
-            Debug.Assert(Width >= Program.formMain.bmpSeparator.Width);
-
-            if ((bmp == null) || (bmp.Width != Width))
-            {
-                bmp?.Dispose();
-
-                bmp = new Bitmap(Width, Program.formMain.bmpSeparator.Height);
-                Graphics gb = Graphics.FromImage(bmp);
-
-                gb.DrawImage(Program.formMain.bmpSeparator, 0, 0, new Rectangle(0, 0, 10, 10), GraphicsUnit.Pixel);
-                gb.DrawImage(Program.formMain.bmpSeparator, bmp.Width - 10, 0, new Rectangle(11, 0, 10, 10), GraphicsUnit.Pixel);
-
-                Rectangle rectBand = new Rectangle(10, 0, 1, 10);
-                for (int i = 10; i < bmp.Width - 10; i++)
-                {
-                    gb.DrawImage(Program.formMain.bmpSeparator, i, 0, rectBand, GraphicsUnit.Pixel);
-                }
-
-                gb.Dispose();
-            }
-
-            base.Draw(g);
-
-            g.DrawImageUnscaled(bmp, Left, Top);
-        }
+        protected override int WidthCap() => 10;
+        protected override Bitmap GetBitmap() => Program.formMain.bmpSeparator;
     }
 }
