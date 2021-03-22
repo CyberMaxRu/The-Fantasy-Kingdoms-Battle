47dd1a01dc4bd62f44e26c4e3429432c4c62b60f
Максим Кузьмин <cybermax@bk.ru>
2021-03-22 19:26:55 +1000

    Класс VCLabelValue.

---
+++ Source/VisualControls/VCLabelValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Визуальный контрол - метка с иконкой, для отображения значения
+    internal sealed class VCLabelValue : VCLabelM2
+    {
+        public VCLabelValue(VisualControl parent, int shiftX, int shiftY, Color foreColor)
+            : base(parent, shiftX, shiftY, Program.formMain.fontMedCaption, foreColor, 16, "")
+        {
+            BitmapList = Program.formMain.ilGui16;
+            StringFormat.Alignment = StringAlignment.Near;
+        }
+    }
+}
