e6783ba5580e44cb7a883b8cbac6fecc8853b33f
Максим Кузьмин <cybermax@bk.ru>
2021-03-25 19:23:58 +1000

    F После клика на исследовании, если нового в ячейке не появилось, подсказка к изученному исследованию продолжает отображаться.

--- Source/VisualControls/VCMenuCell.cs
+++ Source/VisualControls/VCMenuCell.cs
@@ -27,11 +27,18 @@ namespace Fantasy_Kingdoms_Battle
 
         internal override bool PrepareHint()
         {
-            Program.formMain.formHint.AddStep1Header(research.Research.Entity.Name, "", research.Research.Entity.Description);
-            Program.formMain.formHint.AddStep3Requirement(research.GetTextRequirements());
-            Program.formMain.formHint.AddStep4Gold(research.Cost(), research.Cost() <= research.Building.Player.Gold);
+            // После клика на ячейке меню, надо перепоказать подсказку
+            // Если на ячейке исследования больше нет, то сообщаем, что подсказки нет
+            if (research != null)
+            {
+                Program.formMain.formHint.AddStep1Header(research.Research.Entity.Name, "", research.Research.Entity.Description);
+                Program.formMain.formHint.AddStep3Requirement(research.GetTextRequirements());
+                Program.formMain.formHint.AddStep4Gold(research.Cost(), research.Cost() <= research.Building.Player.Gold);
 
-            return true;
+                return true;
+            }
+
+            return false;
         }
 
         internal override void Draw(Graphics g)
