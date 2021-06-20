9cc91c354008e1415f5bb34a429d5ba9eb430ec0
Максим Кузьмин <cybermax@bk.ru>
2021-06-20 20:24:55 +1000

    Доработка главного меню.

--- Source/Forms/WindowMainMenu.cs
+++ Source/Forms/WindowMainMenu.cs
@@ -15,7 +15,7 @@ namespace Fantasy_Kingdoms_Battle
         private VCButton btnPlayerPreferences;
         private VCButton btnGamePreferences;
         private VCButton btnAboutProgram;
-        private VCButton btnExit;
+        private VCButton btnExitToWindows;
 
         public WindowMainMenu()
         {
@@ -27,11 +27,11 @@ namespace Fantasy_Kingdoms_Battle
             btnTournament.Width = Width - 80 - 80;
             btnTournament.Click += BtnTournament_Click;
 
-            btnExit = new VCButton(bmpMenu, 80, bmpMenu.Height - 96, "Выход");
-            btnExit.Width = Width - 80 - 80;
-            btnExit.Click += BtnExit_Click;
+            btnExitToWindows = new VCButton(bmpMenu, 80, bmpMenu.Height - 96, "Выход в Windows");
+            btnExitToWindows.Width = Width - 80 - 80;
+            btnExitToWindows.Click += BtnExitToWindows_Click;
 
-            btnAboutProgram = new VCButton(bmpMenu, 80, btnExit.ShiftY - 40, "О программе");
+            btnAboutProgram = new VCButton(bmpMenu, 80, btnExitToWindows.ShiftY - 40, "О программе");
             btnAboutProgram.Width = Width - 80 - 80;
             btnAboutProgram.Click += BtnAboutProgram_Click;
 
@@ -47,7 +47,7 @@ namespace Fantasy_Kingdoms_Battle
             //btnPlayers.Width = Width - 80 - 80;
             //btnPlayers.Click += BtnPlayers_Click;
 
-            CancelButton = btnExit;
+            CancelButton = btnExitToWindows;
         }
 
         private void BtnPlayerPreferences_Click(object sender, EventArgs e)
@@ -64,9 +64,9 @@ namespace Fantasy_Kingdoms_Battle
             
         }
 
-        private void BtnExit_Click(object sender, EventArgs e)
+        private void BtnExitToWindows_Click(object sender, EventArgs e)
         {
-            //Program.formMain.SetProgrameState(ProgramState.NeedQuit);
+            Program.formMain.SetProgrameState(ProgramState.NeedQuit);
             CloseForm(DialogResult.Abort);
         }
 
@@ -82,7 +82,7 @@ namespace Fantasy_Kingdoms_Battle
 
         private void BtnTournament_Click(object sender, EventArgs e)
         {
-            
+            CloseForm(DialogResult.OK);
         }
     }
 }
