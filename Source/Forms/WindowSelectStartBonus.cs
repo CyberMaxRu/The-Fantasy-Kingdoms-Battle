4788d9f117c317d3cd16ca47394b6672c6bf3266
Максим Кузьмин <cybermax@bk.ru>
2021-06-27 19:23:28 +1000

    Добавлен класс WindowSelectStartBonus.

---
+++ Source/Forms/WindowSelectStartBonus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Окно выбора стартового бонуса
+    internal sealed class WindowSelectStartBonus :  CustomWindow
+    {
+        public WindowSelectStartBonus()
+        {
+
+        }
+    }
+}
