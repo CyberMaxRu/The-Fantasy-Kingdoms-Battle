665783c219cd133201d79d97d3f3799d97a0b837
Максим Кузьмин <cybermax@bk.ru>
2021-08-17 19:20:12 +1000

    Класс VCMapArea.

---
+++ Source/VisualControls/VCMapArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Kingdoms_Battle.Source.VisualControls
+{
+    // Визуальный контрол - область карты
+    internal sealed class VCMapArea : VisualControl
+    {
+        public VCMapArea(VisualControl parent, int shiftX, int shiftY)
+        {
+            Tiles = new VCImage[3, 3];
+
+
+        }
+
+        internal VCImage[,] Tiles { get; }
+    }
+}
