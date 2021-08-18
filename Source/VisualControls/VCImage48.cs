8a324c3938eae607e75ff29c0cbbe02de3b58216
Максим Кузьмин <cybermax@bk.ru>
2021-08-18 12:31:53 +1000

    Добавлен класс VCImage48.

---
+++ Source/VisualControls/VCImage48.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Kingdoms_Battle.Source.VisualControls
+{
+    // Класс для иконок 48 * 48
+    internal class VCImage48 : VCImage
+    {
+        public VCImage48(VisualControl parent, int shiftX, int shiftY) : base(parent, shiftX, shiftY, null, -1)
+        {
+
+        }
+    }
+}
