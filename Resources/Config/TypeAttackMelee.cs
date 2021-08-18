931748cd47062a60f032a5b7602e5f893a5ca031
Максим Кузьмин <cybermax@bk.ru>
2021-08-18 20:44:15 +1000

    Класс TypePerk. Xml-файл с конфигурацией перков. Класс TypeAttackMelee.

---
+++ Resources/Config/TypeAttackMelee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Diagnostics;
+
+namespace Fantasy_Kingdoms_Battle.Resources.Config
+{
+    internal sealed class TypeAttackMelee
+    {
+        public TypeAttackMelee(XmlNode n)
+        {
+
+        }
+    }
+}
---
+++ Resources/Game/Creature/TypePerk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Xml;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Типы перков существ
+    internal sealed class TypePerk : TypeObject
+    {
+        public TypePerk(XmlNode n) : base(n)
+        {
+
+        }
+    }
+}
