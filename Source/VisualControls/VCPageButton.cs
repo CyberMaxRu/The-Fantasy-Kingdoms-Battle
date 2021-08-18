f0fee4f9f149eeb4051895fed5570678a84798dc
Максим Кузьмин <cybermax@bk.ru>
2021-08-18 17:42:42 +1000

    VCPageButton выделен в отдельный модуль.

---
+++ Source/VisualControls/VCPageButton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Класс кнопки для PageControl'а
+    internal sealed class VCPageButton : VCIconButton
+    {
+        public VCPageButton(VisualControl parent, int shiftX, int shiftY, BitmapList bitmapList, int imageIndex, string advice) : base(parent, shiftX, shiftY, bitmapList, imageIndex)
+        {
+            HighlightUnderMouse = true;
+            ShowBorder = true;
+            Advice = advice;
+
+            Page = new VisualControl(parent, 0, NextTop());
+            Page.Visible = false;
+            Page.Click += Page_Click;
+        }
+
+        private void Page_Click(object sender, EventArgs e)
+        {
+            Program.formMain.SelectPlayerObject(null);
+        }
+
+        internal VisualControl Page { get; }
+        internal string Advice { get; }
+        internal PlayerObject SelectedPlayerObject { get; set; }
+    }
+
+}
