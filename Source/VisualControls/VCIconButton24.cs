b5c8257bf970e7e28703b766d8a3528e2025b19d
Максим Кузьмин <cybermax@bk.ru>
2021-08-22 00:32:37 +1000

    Добавлен класс VCIconButton24.

---
+++ Source/VisualControls/VCIconButton24.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    internal sealed class VCIconButton24 : VCImage
+    {
+        public VCIconButton24(VisualControl parent, int shiftX, int shiftY, int imageIndex) : base(parent, shiftX, shiftY, Program.formMain.ilGui24, imageIndex)
+        {
+            HighlightUnderMouse = true;
+            PlaySoundOnClick = true;
+        }
+
+        internal static VCIconButton24 CreateButton(VisualControl parent, int shiftX, int shiftY, int imageIndex, EventHandler onClick)
+        {
+            VCIconButton24 button = new VCIconButton24(parent, shiftX, shiftY, imageIndex);
+            button.Click += onClick;
+
+            return button;
+        }
+
+        internal override void MouseDown()
+        {
+            base.MouseDown();
+
+            //if (ImageIsEnabled)
+            //    Program.formMain.PlayPushButton();
+        }
+    }
+}
