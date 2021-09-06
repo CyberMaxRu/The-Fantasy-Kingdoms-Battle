e36c74a9c2ec540ae16bcf1c6c4dca2eb7d73a98
Максим Кузьмин <cybermax@bk.ru>
2021-09-06 12:42:23 +1000

    Классы KindCreature и StateCreature перемещены в другую папку. StateCreature теперь потомок от DescriptorEntity.

---
+++ Source/Descriptors/Creature/StateCreature.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Diagnostics;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Состояния существа
+    internal enum NameStateCreature { Nothing, DoScoutFlag, DoAttackFlag, DoDefenseFlag, BattleWithPlayer, InHome, Therapy, King, Advisor, Captain, Treasurer };
+
+    // Класс описания состояния существ
+    internal sealed class StateCreature : DescriptorEntity
+    {
+        public StateCreature(XmlNode n) : base(n)
+        {
+            foreach (StateCreature sc in Config.StatesCreature)
+            {
+                Debug.Assert(sc.ID != ID);
+                Debug.Assert(sc.Name != Name);
+                Debug.Assert(sc.Description != Description);
+                Debug.Assert(sc.ImageIndex != ImageIndex);
+            }
+        }
+    }
+}
