5808e285f00dd70743bb120fcdcbcc3d4a433ff4
Максим Кузьмин <cybermax@bk.ru>
2021-09-07 12:58:05 +1000

    DescriptorReward потомок от Descriptor.

--- Source/Descriptors/Creature/DescriptorReward.cs
+++ Source/Descriptors/Creature/DescriptorReward.cs
@@ -5,16 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.Xml;
+using static Fantasy_Kingdoms_Battle.XmlUtils;
 
 namespace Fantasy_Kingdoms_Battle
 {
     // Класс типы награды
-    internal sealed class DescriptorReward
+    internal sealed class DescriptorReward : Descriptor
     {
-        public DescriptorReward(XmlNode n)
+        public DescriptorReward(XmlNode n) : base()
         {
-            Gold = XmlUtils.GetInteger(n, "Gold");
-            Greatness = XmlUtils.GetInteger(n, "Greatness");
+            Gold = GetInteger(n, "Gold");
+            Greatness = GetInteger(n, "Greatness");
 
             Debug.Assert(Gold >= 0);
             Debug.Assert(Gold <= 50_000);
