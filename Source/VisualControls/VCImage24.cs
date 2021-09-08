5a5ca31e21cf956e70e655926e07cb97a6e72944
Максим Кузьмин <cybermax@bk.ru>
2021-09-08 12:35:21 +1000

    Класс VCImage24.

---
+++ Source/VisualControls/VCImage24.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    internal sealed class VCImage24 : VCImage
+    {
+        public VCImage24(VisualControl parent, int shiftX, int shiftY, int imageIndex) : base(parent, shiftX, shiftY, Program.formMain.ilGui24, imageIndex)
+        {
+
+        }
+    }
+}
