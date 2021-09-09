e9930c9f3383ac6a0fdb85e15a049aea6d950be8
Максим Кузьмин <cybermax@bk.ru>
2021-09-09 12:42:09 +1000

    Если сооружение можно строить/улучшать, цвет цены зеленый иначе серый.

--- Source/Panels/PanelConstruction.cs
+++ Source/Panels/PanelConstruction.cs
@@ -187,6 +187,7 @@ namespace Fantasy_Kingdoms_Battle
                             btnBuildOrUpgrade.Text = Construction.CostBuyOrUpgrade().ToString();
                             btnBuildOrUpgrade.ImageIndex = FormMain.Config.Gui48_LevelUp;
                             btnBuildOrUpgrade.ImageIsEnabled = Construction.CheckRequirements();
+                            btnBuildOrUpgrade.Color = btnBuildOrUpgrade.ImageIsEnabled ? Color.LimeGreen : Color.Gray;
                         }
                         else
                         {
@@ -199,6 +200,7 @@ namespace Fantasy_Kingdoms_Battle
                         btnBuildOrUpgrade.Text = Construction.CostBuyOrUpgrade().ToString();
                         btnBuildOrUpgrade.ImageIndex = Construction.TypeConstruction.ID != FormMain.Config.IDHolyPlace ? FormMain.Config.Gui48_Build : FormMain.Config.Gui48_Temple;
                         btnBuildOrUpgrade.ImageIsEnabled = (Construction.TypeConstruction.MaxLevel > 0) ? Construction.CheckRequirements() : true;
+                        btnBuildOrUpgrade.Color = btnBuildOrUpgrade.ImageIsEnabled ? Color.LimeGreen : Color.Gray;
                     }
                 }
                 else
