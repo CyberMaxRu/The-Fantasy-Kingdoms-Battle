c64f57f79fea7c1991e936973a775c0ee62d960e
Максим Кузьмин <cybermax@bk.ru>
2021-09-16 16:45:59 +1000

    Доработка класса Perk.

--- Source/Lobby/Perk.cs
+++ Source/Lobby/Perk.cs
@@ -9,21 +9,24 @@ namespace Fantasy_Kingdoms_Battle
 {
     internal sealed class Perk : EntityForCreature
     {
-        public Perk(Creature creature, DescriptorPerk descriptor) : base(creature)
+        public Perk(Creature creature, DescriptorPerk descriptor, Entity owner, Creature signer = null, int counter = 0) : base(creature)
         {
             Descriptor = descriptor;
+            Owner = owner;
+            Signer = signer;
+            Counter = counter;
         }
 
         internal DescriptorPerk Descriptor { get; }
+        internal int Counter { get; private set; }// Счетчик действия
+        internal Entity Owner { get; }// Владелец перка
+        internal Creature Signer { get; }// Кто подписал (если указывается)
 
-        internal override int GetImageIndex()
-        {
-            throw new NotImplementedException();
-        }
+        internal override int GetImageIndex() => Descriptor.ImageIndex;
 
         internal override void PrepareHint()
         {
-            throw new NotImplementedException();
+            Program.formMain.formHint.AddStep1Header(Descriptor.Name, "", Descriptor.Description, Descriptor.ImageIndex, false);
         }
     }
 }
