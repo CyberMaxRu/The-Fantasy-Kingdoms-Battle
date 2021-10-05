0020ef3bdf4a851700b8f7aa813f6dfdbde67ef9
Максим Кузьмин <cybermax@bk.ru>
2021-10-05 17:30:34 +1000

    Класс VCIconAndValue.

---
+++ Source/VisualControls/VCIconAndDigitValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    internal sealed class VCIconAndDigitValue : VCLabel
+    {
+        public VCIconAndDigitValue(VisualControl parent, int shiftX, int shiftY, int width, int imageIndex)
+            : base(parent, shiftX, shiftY, Program.formMain.fontMedCaption, Color.White, 24, "")
+        {
+            BitmapList = Program.formMain.ilGui16;
+            StringFormat.Alignment = StringAlignment.Near;
+            ShiftImage = new Point(4, 4);
+            TopMargin = 2;
+            LeftMargin = 0;
+            RightMargin = 4;
+            ImageIndex = imageIndex;
+            StringFormat.Alignment = StringAlignment.Far;
+            Width = width;
+        }
+
+        internal override void DrawBackground(Graphics g)
+        {
+            base.DrawBackground(g);
+
+            Program.formMain.bbIcon16.DrawBorder(g, Left, Top, Width, Program.formMain.bbIcon16.Height);
+        }
+    }
+}
