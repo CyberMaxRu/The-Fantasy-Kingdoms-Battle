5f1114176db78d5f2547de23408fa876de864ab9
Максим Кузьмин <cybermax@bk.ru>
2021-10-21 23:04:02 +1000

    F Сработка Assert.

--- Source/Panels/PanelLocationInfo.cs
+++ Source/Panels/PanelLocationInfo.cs
@@ -10,7 +10,10 @@ namespace Fantasy_Kingdoms_Battle
     {
         public PanelLocationInfo(VisualControl parent, int shiftX, int shiftY) : base(parent, shiftX, shiftY)
         {
+            pageControl.AddTab("История", FormMain.Config.Gui48_Book, null);
 
+            pageControl.ApplyMinSize();
+            Width = pageControl.Width + FormMain.Config.GridSize * 2;
         }
 
         internal Location Location { get => Entity as Location; }
@@ -18,5 +21,12 @@ namespace Fantasy_Kingdoms_Battle
         protected override int GetImageIndex() => Location.Descriptor.ImageIndex;
         protected override bool ImageIsEnabled() => true;// Location.Level > 0;
         protected override string GetCaption() => Location.Descriptor.Name;
+
+        internal override void ArrangeControls()
+        {
+            base.ArrangeControls();
+
+            pageControl.Height = Height - pageControl.ShiftY - FormMain.Config.GridSize;
+        }
     }
 }
