88eb63dde39fede65d90613908e6438fc77bb17e
Максим Кузьмин <cybermax@bk.ru>
2021-11-23 18:41:32 +1000

    Доработка DescriptorSelling.

--- Source/Descriptors/DescriptorSelling.cs
+++ Source/Descriptors/DescriptorSelling.cs
@@ -22,24 +22,24 @@ namespace Fantasy_Kingdoms_Battle
             Gold = GetIntegerNotNull(n, "Gold");
             Quantity = GetIntegerNotNull(n, "Quantity");
             Duration = GetIntegerNotNull(n, "Duration");
-            MovePoints = GetIntegerNotNull(n, "MovePoints");
+            DaysProcessing = GetIntegerNotNull(n, "DaysProcessing");
             IntervalRefresh = (IntervalRefresh)Enum.Parse(typeof(IntervalRefresh), GetStringNotNull(n, "IntervalRefresh"));
 
             Debug.Assert(Gold >= 0, $"У {entity} отрицательное число стоимости ({Gold}).");
-            Debug.Assert(Gold <= 100_000);
+            Debug.Assert(Gold <= 100_000, $"У {entity} слишком больше число стоимости ({Gold}).");
             Debug.Assert(Quantity > 0, $"У {entity} ошибк в количестве ({Quantity}).");
             Debug.Assert(Quantity <= 1_000, $"У {entity} ошибк в количестве ({Quantity}).");
-            Debug.Assert(Duration > 0, $"У {entity} ошибк в количестве ({Duration}).");
-            Debug.Assert(Duration <= 1_000, $"У {entity} ошибк в количестве ({Duration}).");
-            Debug.Assert(MovePoints > 0, $"У {entity} ошибк в количестве ({MovePoints}).");
-            Debug.Assert(MovePoints <= 1_000, $"У {entity} ошибк в количестве ({MovePoints}).");
+            Debug.Assert(Duration > 0, $"У {entity} ошибк в длительности ({Duration}).");
+            Debug.Assert(Duration <= 1_000, $"У {entity} ошибк в длительности ({Duration}).");
+            Debug.Assert(DaysProcessing >= 0, $"У {entity} ошибк в количестве дней ({DaysProcessing}).");
+            Debug.Assert(DaysProcessing <= 100, $"У {entity} ошибк в количестве дней ({DaysProcessing}).");
         }
 
         internal DescriptorWithID Entity { get; }// Для какой активной сущности
-        internal int Gold { get; }// Количество требуемых строителей
-        internal int Quantity { get; }// Количество требуемых строителей
-        internal int Duration { get; }// Количество требуемых строителей
-        internal int MovePoints { get; }// Количество требуемых строителей
-        internal IntervalRefresh IntervalRefresh { get; }
+        internal int Gold { get; }// Количество золота для покупки
+        internal int Quantity { get; }// Количество товара
+        internal int Duration { get; }// Длительность существования товара
+        internal int DaysProcessing { get; }// Количество дней для покупки товара
+        internal IntervalRefresh IntervalRefresh { get; }// Интервал обновления товара в сооружении
     }
 }
