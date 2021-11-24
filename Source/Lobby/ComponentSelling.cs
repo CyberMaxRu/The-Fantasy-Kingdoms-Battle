11c88ff618b019050c8474f9dd396daab4472df7
Максим Кузьмин <cybermax@bk.ru>
2021-11-25 00:29:09 +1000

    Комментарии.

--- Source/Lobby/ComponentSelling.cs
+++ Source/Lobby/ComponentSelling.cs
@@ -14,12 +14,12 @@ namespace Fantasy_Kingdoms_Battle.Source.Lobby
         }
 
         internal DescriptorComponentSelling Descriptor { get; }
-        internal int RestQuantity { get; set; }
-        internal int CountLife { get; set; }
-        internal bool Enabled { get; set; }
-        internal int TotalSellQuantity { get; set; }// Количество продаж
+        internal int RestQuantity { get; set; }// Осталось количество товара/услуг
+        internal int CountLife { get; set; }// Счетчик дней жизни товара/услуги
+        internal bool Enabled { get; set; }// Товар/услуга разрешены к продаже
+        internal int TotalSellQuantity { get; set; }// Всего количество продаж
         internal int TotalSellGold { get; set; }// Суммма продаж (без налога)
-        internal int TotalDuty { get; set; }// Налог с продаж
+        internal int TotalDuty { get; set; }// Всего начислено налога с продаж
 
         internal void Reset()
         {
