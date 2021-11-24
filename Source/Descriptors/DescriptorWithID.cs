b564c6f00de714c677e8df226b4fb579cd2187cf
Максим Кузьмин <cybermax@bk.ru>
2021-11-24 22:54:27 +1000

    Метод GetStringFromXmlNode.

--- Source/Descriptors/DescriptorWithID.cs
+++ Source/Descriptors/DescriptorWithID.cs
@@ -60,7 +60,20 @@ namespace Fantasy_Kingdoms_Battle
             Assert(value <= maxValue, $"{ID}: значение ({value}) поля {name} больше допустимого ({maxValue}).");
 
             return value;
-        }        
+        }
+
+        internal string GetStringFromXmlNode(XmlNode n, string name, bool required = true)
+        {
+            XmlNode nn = n.SelectSingleNode(name);
+            if (required)
+                Assert(nn != null, $"{ID}: поле {name} отсутствует.");
+
+            string s = nn?.InnerText;
+            if (required)
+                Assert(s.Length > 0, $"{ID}: поле {name} не имеет значения.");
+
+            return s;
+        }
 
         internal override void TuneLinks()
         {
