3cfc8e17195233444fbb258054ce56805556fb6e
Максим Кузьмин <cybermax@bk.ru>
2021-11-25 00:29:32 +1000

    Улучшение кода.

--- Source/Lobby/ComponentSelling.cs
+++ Source/Lobby/ComponentSelling.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Fantasy_Kingdoms_Battle.Source.Lobby
+﻿namespace Fantasy_Kingdoms_Battle
 {
     internal sealed class ComponentSelling
     {
