542f6f1d778ed0dce1aac6b48d9107a06cacef02
Максим Кузьмин <cybermax@bk.ru>
2021-11-24 22:42:38 +1000

    Упрощение класса DescriptorComponentCreating.

--- Source/Descriptors/DescriptorComponentCreating.cs
+++ Source/Descriptors/DescriptorComponentCreating.cs
@@ -1,35 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml;
-using System.Diagnostics;
-using System.Drawing;
-using System.Windows.Forms;
-using static Fantasy_Kingdoms_Battle.XmlUtils;
+﻿using System.Xml;
 
 namespace Fantasy_Kingdoms_Battle
 {
-    internal class DescriptorComponentCreating : Descriptor
+    // Класс описателя с информацией о создании сущности
+    internal sealed class DescriptorComponentCreating : Descriptor
     {
         public DescriptorComponentCreating(DescriptorWithID entity, XmlNode n) : base()
         {
-            Entity = entity;
-
-            Builders = GetInteger(n, "Builders");
-            DaysProcessing = GetInteger(n, "DaysProcessing");
+            Builders = entity.GetIntegerFromXmlNode(n, "Builders", 0, 10);
+            DaysProcessing = entity.GetIntegerFromXmlNode(n, "DaysProcessing", 0, 100);
             CostResources = new ListBaseResources(n.SelectSingleNode("Cost"));
-            Requirements = new ListDescriptorRequirements(Entity, n.SelectSingleNode("Requirements"));
-
-            Debug.Assert(DaysProcessing >= 0, $"У {entity} отрицательное число дней создания ({DaysProcessing}).");
-            Debug.Assert(DaysProcessing <= 100);
+            Requirements = new ListDescriptorRequirements(entity, n.SelectSingleNode("Requirements"));
         }
 
-        internal DescriptorWithID Entity { get; }// Для какой активной сущности
         internal int Builders { get; }// Количество требуемых строителей
         internal int DaysProcessing { get; }// Количество дней для выполнения действия
-        internal ListBaseResources CostResources { get; }// Стоимость
+        internal ListBaseResources CostResources { get; }// Стоимость (в базовых ресурсах)
         internal ListDescriptorRequirements Requirements { get; }// Список требований для выполнения действия
 
         internal override void TuneLinks()
