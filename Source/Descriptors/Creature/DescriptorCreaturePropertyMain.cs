a15d3db97b26d75a7bbe47b2b397f9e27bd02a2d
Максим Кузьмин <cybermax@bk.ru>
2021-11-29 19:58:20 +1000

    В DescriptorCreaturePropertyMain добавлено свойство ChangePerLevel.

--- Source/Descriptors/Creature/DescriptorCreaturePropertyMain.cs
+++ Source/Descriptors/Creature/DescriptorCreaturePropertyMain.cs
@@ -11,15 +11,19 @@ namespace Fantasy_Kingdoms_Battle
         {
             MinValueOnHire = GetIntegerNotNull(n, "MinValueOnHire");
             MaxValueOnHire = GetIntegerNotNull(n, "MaxValueOnHire");
+            ChangePerLevel = GetInteger(n, "ChangePerLevel");
 
             Debug.Assert(MinValueOnHire >= -Config.MaxValueProperty);
             Debug.Assert(MinValueOnHire <= Config.MaxValueProperty);
             Debug.Assert(MaxValueOnHire >= -Config.MaxValueProperty);
             Debug.Assert(MaxValueOnHire <= Config.MaxValueProperty);
             Debug.Assert(MinValueOnHire <= MaxValueOnHire);
+            Debug.Assert(ChangePerLevel >= -Config.MaxValueProperty);
+            Debug.Assert(ChangePerLevel <= Config.MaxValueProperty);
         }
 
         internal int MinValueOnHire { get; }// Минимальное значение при найме
         internal int MaxValueOnHire { get; }// Максимальное значение при найме
+        internal int ChangePerLevel { get; }// Изменение при повышении уровня
     }
 }
