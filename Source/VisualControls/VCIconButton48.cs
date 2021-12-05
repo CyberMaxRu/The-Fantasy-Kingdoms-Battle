08428a7cafce543059110221da1361b6adcc2293
Максим Кузьмин <cybermax@bk.ru>
2021-12-05 20:45:20 +1000

    F Звук двойного клика в VCIconButton48.

--- Source/VisualControls/VCIconButton48.cs
+++ Source/VisualControls/VCIconButton48.cs
@@ -20,13 +20,5 @@ namespace Fantasy_Kingdoms_Battle
 
             base.Draw(g);
         }
-
-        internal override void MouseDown()
-        {
-            base.MouseDown();
-
-            if (ImageIsEnabled)
-                Program.formMain.PlayPushButton();
-        }
     }
 }
