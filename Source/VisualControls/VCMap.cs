ae94891e59965ab7850aa8cfa1455ffaf1caff5f
Максим Кузьмин <cybermax@bk.ru>
2021-12-08 21:16:38 +1000

    Класс VCMap.

---
+++ Source/VisualControls/VCMap.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using System.Diagnostics;
+using static Fantasy_Kingdoms_Battle.Utils;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    internal sealed class VCMap : VisualControl
+    {
+        private Bitmap bmp;
+        private Point shiftBitmap;
+        private Rectangle windowDraw = new Rectangle();
+
+        public VCMap(VisualControl parent, int shiftX, int shiftY, string filenameBitmap) : base(parent, shiftX, shiftY)
+        {
+            bmp = Program.formMain.LoadBitmap(filenameBitmap, @"Icons\Conq");
+        }
+
+        internal override void Draw(Graphics g)
+        {
+            Debug.Assert(bmp != null);
+            UpdateWindow();
+
+            base.Draw(g);
+
+            g.DrawImage(bmp, Left, Top, windowDraw, GraphicsUnit.Pixel);
+        }
+
+        private void UpdateWindow()
+        {
+            if ((windowDraw.Left != shiftBitmap.X) || (windowDraw.Top != shiftBitmap.Y) || (windowDraw.Width != Width) || (windowDraw.Height !=  Height))
+                windowDraw = new Rectangle(shiftBitmap.X, shiftBitmap.Y, Width, Height);
+        }
+
+        internal override void MouseMove(Point p, bool leftDown, bool rightDown)
+        {
+            base.MouseMove(p, leftDown, rightDown);
+
+            //if (dow)
+        }
+    }
+}
