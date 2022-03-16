b3f66503a1433fe92b7a098e547ad5b3cf4a5e8c
Максим Кузьмин <cybermax@bk.ru>
2022-03-16 21:21:59 +1000

    Сборка 0.3.18.

--- FormMain.cs
+++ FormMain.cs
@@ -20,8 +20,8 @@ namespace Fantasy_Kingdoms_Battle
     public partial class FormMain : Form
     {
         internal const string NAME_PROJECT = "The Fantasy Kingdoms Battle";
-        internal const string VERSION = "0.3.17";
-        internal const string DATE_VERSION = "06.11.2021";
+        internal const string VERSION = "0.3.18";
+        internal const string DATE_VERSION = "16.03.2022";
         private const string VERSION_POSTFIX = "в разработке";
 
         internal ProgramState ProgramState { get; private set; } = ProgramState.Started;
