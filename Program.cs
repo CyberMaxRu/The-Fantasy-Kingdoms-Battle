f3df21e295d637ab5e96c4f78c8382ec1f6b0f3a
Максим Кузьмин <cybermax@bk.ru>
2022-05-22 22:44:06 +1000

    При запуске под из среды не перехватываются исключения. Это упрощает отладку.

--- Program.cs
+++ Program.cs
@@ -19,39 +19,52 @@ namespace Fantasy_Kingdoms_Battle
         [STAThread]
         static void Main()
         {
+            bool debugMode = false;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            try
+            // Настройка переменной с папкой ресурсов
+            WorkFolder = Environment.CurrentDirectory;
+
+            if (WorkFolder.Contains("Debug"))
             {
-                // Настройка переменной с папкой ресурсов
-                WorkFolder = Environment.CurrentDirectory;
+                WorkFolder = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 9);
+                debugMode = true;
+            }
+            else if (WorkFolder.Contains("Release"))
+                WorkFolder = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 11);
+            else
+                WorkFolder += @"\";
 
-                if (WorkFolder.Contains("Debug"))
-                    WorkFolder = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 9);
-                else if (WorkFolder.Contains("Release"))
-                    WorkFolder = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 11);
-                else
-                    WorkFolder += @"\";
-
-                FolderResources = Directory.Exists(WorkFolder + @"User_mods\Main") ? WorkFolder + @"User_mods\Main\" : WorkFolder + @"Resources\";
+            FolderResources = Directory.Exists(WorkFolder + @"User_mods\Main") ? WorkFolder + @"User_mods\Main\" : WorkFolder + @"Resources\";
+            if (debugMode)
+            {
                 new FormMain();
                 Application.Run(formMain);
             }
-            catch (Exception exc)
+            else
             {
-                string stackTrace = exc.StackTrace.Replace(@"F:\Projects\C-Sharp\Fantasy King's Battle\", "");
+                try
+                {
+                    new FormMain();
+                    Application.Run(formMain);
+                }
+                catch (Exception exc)
+                {
+                    string stackTrace = exc.StackTrace.Replace(@"F:\Projects\C-Sharp\Fantasy King's Battle\", "");
 
-                StreamWriter sw = File.AppendText(Environment.CurrentDirectory + @"\Exceptions.log");
-                sw.WriteLine(DateTime.Now.ToString() + ": v" + Application.ProductVersion);
-                sw.WriteLine(exc.Message);
-                sw.WriteLine(stackTrace);
-                sw.Close();
-                sw.Dispose();
+                    StreamWriter sw = File.AppendText(Environment.CurrentDirectory + @"\Exceptions.log");
+                    sw.WriteLine(DateTime.Now.ToString() + ": v" + Application.ProductVersion);
+                    sw.WriteLine(exc.Message);
+                    sw.WriteLine(stackTrace);
+                    sw.Close();
+                    sw.Dispose();
 
-                MessageBox.Show("Произошло исключение: "
-                    + (exc.Message.Length > 0 ? Environment.NewLine + Environment.NewLine + exc.Message : "") + Environment.NewLine + Environment.NewLine + stackTrace, FormMain.NAME_PROJECT + " " + FormMain.VERSION, MessageBoxButtons.OK, MessageBoxIcon.Error);
-                //Debug.Fail(exc.Message);
-                Environment.Exit(-1);
+                    MessageBox.Show("Произошло исключение: "
+                        + (exc.Message.Length > 0 ? Environment.NewLine + Environment.NewLine + exc.Message : "") + Environment.NewLine + Environment.NewLine + stackTrace, FormMain.NAME_PROJECT + " " + FormMain.VERSION, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    //Debug.Fail(exc.Message);
+                    Environment.Exit(-1);
+                }
             }
         }
 
