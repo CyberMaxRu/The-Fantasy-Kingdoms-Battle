5f0f8567c83280dfb564d86ec8ba205878849782
Максим Кузьмин <cybermax@bk.ru>
2022-05-22 19:36:02 +1000

    F В PanelConstructionInfo у Пути отображается не иконка локации.

--- Source/Panels/PanelConstructionInfo.cs
+++ Source/Panels/PanelConstructionInfo.cs
@@ -229,7 +229,7 @@ namespace Fantasy_Kingdoms_Battle
             }
         }
 
-        protected override int GetImageIndex() => Construction.TypeConstruction.ImageIndex;
+        protected override int GetImageIndex() => Construction.GetImageIndex();
         protected override bool ImageIsEnabled() => Construction.Level > 0;
         protected override string GetCaption() => Construction.TypeConstruction.Name;
 
