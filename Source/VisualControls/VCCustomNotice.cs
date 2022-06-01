ea5fa4278561bb60b4aebd3bf1d23da36da55dd9
Максим Кузьмин <cybermax@bk.ru>
2022-06-01 11:46:24 +1000

    Изменен цвет у текста извещения с серого на белый.

--- Source/VisualControls/VCCustomNotice.cs
+++ Source/VisualControls/VCCustomNotice.cs
@@ -20,9 +20,9 @@ namespace Fantasy_Kingdoms_Battle
         {
             Cell = new VCCellSimple(this, 0, 3);
 
-            lblCaption = new VCLabel(this, Cell.NextLeft(), 4, Program.formMain.fontMedCaptionC, Color.Gray, 16, "");
+            lblCaption = new VCLabel(this, Cell.NextLeft(), 4, Program.formMain.fontMedCaptionC, Color.White, 16, "");
 
-            lblText = new VCLabel(this, lblCaption.ShiftX, 27, Program.formMain.fontMedCaptionC, Color.Gray, 16, "");
+            lblText = new VCLabel(this, lblCaption.ShiftX, 27, Program.formMain.fontMedCaptionC, Color.White, 16, "");
             Height = 54;
             Width = width;
 
