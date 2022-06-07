0d433af8d5f48390f9491ccad2582a33486eb714
Максим Кузьмин <cybermax@bk.ru>
2022-06-07 23:01:30 +1000

    Добавлены классы для загрузки информации о миссии.

--- Source/Mission/Mission.cs
+++ Source/Mission/Mission.cs
@@ -6,20 +6,30 @@ using System.Threading.Tasks;
 using System.Xml;
 using static Fantasy_Kingdoms_Battle.XmlUtils;
 
-namespace Fantasy_Kingdoms_Battle.Source.Mission
+namespace Fantasy_Kingdoms_Battle
 {
     // Класс одиночной миссии
     sealed internal class Mission
     {
-        public Mission(XmlNode n)
+        public Mission(string filename)
         {
+            XmlDocument xml = new XmlDocument();
+            xml.Load(filename);
+            XmlNode n = xml.SelectSingleNode("Mission");
+
             ID = GetStringNotNull(n, "ID");
             Name = GetStringNotNull(n, "Name");
             Description = GetStringNotNull(n, "Description");
+
+
         }
 
         internal string ID { get; }// Уникальный идентификатор миссии
         internal string Name { get; }
         internal string Description { get; }
+
+        internal List<MissionPlayer> Players { get; } = new List<MissionPlayer>();
+        internal List<MissionMessage> Messages { get; } = new List<MissionMessage>();
+        internal List<MissionQuest> Quests { get; } = new List<MissionQuest>();
     }
 }
