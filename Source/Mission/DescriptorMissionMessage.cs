d5a1be1784fb931c7bb1c2ce738e110cc044a5e0
Максим Кузьмин <cybermax@bk.ru>
2022-06-17 22:20:26 +1000

    Сообщения миссии теперь состоят из частей.

--- Source/Mission/DescriptorMissionMessage.cs
+++ Source/Mission/DescriptorMissionMessage.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
 using static Fantasy_Kingdoms_Battle.XmlUtils;
+using static Fantasy_Kingdoms_Battle.Utils;
 
 namespace Fantasy_Kingdoms_Battle
 {
@@ -13,11 +14,26 @@ namespace Fantasy_Kingdoms_Battle
         public DescriptorMissionMessage(XmlNode n)
         {
             Turn = GetIntegerNotNull(n, "Turn");
+
+            // Загружаем части сообщения
+            foreach (XmlNode np in n.SelectNodes("Part"))
+            {
+                Parts.Add(new DescriptorMissionMessagePart(np));
+            }
+        }
+
+        internal int Turn { get; }
+        internal List<DescriptorMissionMessagePart> Parts { get; } = new List<DescriptorMissionMessagePart>();
+    }
+
+    internal sealed class DescriptorMissionMessagePart
+    {
+        public DescriptorMissionMessagePart(XmlNode n)
+        {
             From = GetStringNotNull(n, "From");
             Text = GetStringNotNull(n, "Text");
         }
 
-        internal int Turn { get; }
         internal string From { get; }
         internal string Text { get; }
     }
