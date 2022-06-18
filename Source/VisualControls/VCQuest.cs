62fad864cf2404eb964ca0b5d0b3f1454ca7c213
Максим Кузьмин <cybermax@bk.ru>
2022-06-18 16:48:46 +1000

    Добавлен класс VCQuest.

---
+++ Source/VisualControls/VCQuest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Fantasy_Kingdoms_Battle
+{
+    // Класс контрола для задания
+    internal sealed class VCQuest : VisualControl
+    {
+        public VCQuest() : base()
+        {
+
+        }
+    }
+}
