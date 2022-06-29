4122a8567cb49e6981147e0a9fc4b9cf00b5f696
Максим Кузьмин <cybermax@bk.ru>
2022-06-29 11:57:19 +1000

    Для DescriptorComponentCreating сделано считывание ConstructionPoints явным образом, вместо подставления Durability.

--- Source/Descriptors/DescriptorComponentCreating.cs
+++ Source/Descriptors/DescriptorComponentCreating.cs
@@ -1,24 +1,23 @@
 ﻿using System.Xml;
 using static Fantasy_Kingdoms_Battle.Utils;
+using static Fantasy_Kingdoms_Battle.XmlUtils;
 
 namespace Fantasy_Kingdoms_Battle
 {
     // Класс описателя с информацией о создании сущности
     internal sealed class DescriptorComponentCreating : Descriptor
     {
-        private int constructionPoints;
-
         public DescriptorComponentCreating(DescriptorWithID entity, XmlNode n) : base()
         {
             Entity = entity;
-            cons
+            ConstructionPoints = GetInteger(n, "ConstructionPoints");
             CostResources = new ListBaseResources(n.SelectSingleNode("Cost"));
             Requirements = new ListDescriptorRequirements(entity, n.SelectSingleNode("Requirements"));
         }
 
         internal DescriptorWithID Entity { get; }
-        //internal int ConstructionPoints { get; }
-        internal int CalcConstructionPoints(Player p) => p.CheatingIgnoreBuilders ? 0 : constructionPoints;// Количество требуемых очков строительства
+        internal int ConstructionPoints { get; }
+        internal int CalcConstructionPoints(Player p) => p.CheatingIgnoreBuilders ? 0 : ConstructionPoints;// Количество требуемых очков строительства
         internal ListBaseResources CostResources { get; }// Стоимость (в базовых ресурсах)
         internal ListDescriptorRequirements Requirements { get; }// Список требований для выполнения действия
         internal int DaysProcessing { get; } = 1;
@@ -26,11 +25,6 @@ namespace Fantasy_Kingdoms_Battle
         internal override void TuneLinks()
         {
             base.TuneLinks();
-
-            if (Entity is DescriptorConstructionStructure dcs)
-            {
-                constructionPoints = dcs.Durability;
-            }
 
             Requirements.TuneLinks();
         }
