29e0c9700f6b5e6d09ba338314bb9595fb5bf949
Максим Кузьмин <cybermax@bk.ru>
2022-06-29 14:40:21 +1000

    DaysProcessing заменено на ResearchPoints. Удалена сущность Builder.

--- Source/Descriptors/DescriptorComponentCreating.cs
+++ Source/Descriptors/DescriptorComponentCreating.cs
@@ -11,12 +11,21 @@ namespace Fantasy_Kingdoms_Battle
         {
             Entity = entity;
             ConstructionPoints = GetInteger(n, "ConstructionPoints");
+            ResearchPoints = GetInteger(n, "ResearchPoints");
             CostResources = new ListBaseResources(n.SelectSingleNode("Cost"));
             Requirements = new ListDescriptorRequirements(entity, n.SelectSingleNode("Requirements"));
+
+            Assert(ConstructionPoints >= 0, $"ID: {entity.ID}, ConstructionPoints: {ConstructionPoints}");
+            Assert(ConstructionPoints <= FormMain.Config.DefaultConstructionPoints * 50, $"ID: {entity.ID}, ConstructionPoints: {ConstructionPoints}");
+            Assert(ResearchPoints >= 0, $"ID: {entity.ID}, ResearchPoints: {ResearchPoints}");
+            Assert(ResearchPoints <= FormMain.Config.DefaultResearchPoints * 50, $"ID: {entity.ID}, ResearchPoints: {ResearchPoints}");
+
+            //Assert(((ConstructionPoints > 0) && (ResearchPoints == 0)) || ((ConstructionPoints == 0) && (ResearchPoints > 0)), $"ID: {entity.ID}, ConstructionPoints: {ConstructionPoints}, ResearchPoints: {ResearchPoints}");
         }
 
         internal DescriptorWithID Entity { get; }
-        internal int ConstructionPoints { get; }
+        internal int ConstructionPoints { get; }// Требуемые очки строительства
+        internal int ResearchPoints { get; }// Требуемые очки исследования
         internal int CalcConstructionPoints(Player p) => p.CheatingIgnoreBuilders ? 0 : ConstructionPoints;// Количество требуемых очков строительства
         internal ListBaseResources CostResources { get; }// Стоимость (в базовых ресурсах)
         internal ListDescriptorRequirements Requirements { get; }// Список требований для выполнения действия
