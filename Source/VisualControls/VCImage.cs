6ad6ddaeab203ed1d4d575370f69abcb54127c73
Максим Кузьмин <cybermax@bk.ru>
2022-07-05 15:53:51 +1000

    DaysExecuting поднято в VCImage. В VCEntityInQueue количество оставишихся дней.

--- Source/VisualControls/VCImage.cs
+++ Source/VisualControls/VCImage.cs
@@ -7,6 +7,8 @@ namespace Fantasy_Kingdoms_Battle
     // Визуальный контрол - иконка
     internal class VCImage : VisualControl
     {
+        private VCLabel lblDaysExecuting;
+
         public VCImage(VisualControl parent, int shiftX, int shiftY, BitmapList bitmapList, int imageIndex) : base(parent, shiftX, shiftY)
         {
             BitmapList = bitmapList;
@@ -14,12 +16,20 @@ namespace Fantasy_Kingdoms_Battle
 
             Width = BitmapList.Size.Width;
             Height = BitmapList.Size.Height;
+
+            lblDaysExecuting = new VCLabel(this, 4, 1, Program.formMain.fontSmallC, Color.SkyBlue, 16, "");
+            lblDaysExecuting.StringFormat.LineAlignment = StringAlignment.Near;
+            lblDaysExecuting.StringFormat.Alignment = StringAlignment.Near;
+            lblDaysExecuting.Width = Width - 4;
+            lblDaysExecuting.Visible = false;
+            lblDaysExecuting.ManualDraw = true;
         }
 
         internal BitmapList BitmapList { get; set; }
         internal int ImageIndex { get; set; }
         internal bool ImageIsEnabled { get; set; } = true;
         internal bool HighlightUnderMouse { get; set; } = false;
+        internal string DaysExecuting { get; set; } = "";
 
         internal override void MouseEnter(bool leftButtonDown)
         {
@@ -45,6 +55,14 @@ namespace Fantasy_Kingdoms_Battle
             if ((Visible || ManualDraw) && (ImageIndex != -1))
             {
                 BitmapList.DrawImage(g, ImageIndex, /*UseFilter*/ ImageIsEnabled, HighlightUnderMouse && MouseOver && !MouseClicked, Left, Top);
+
+                // Дней выполнения
+                if (DaysExecuting.Length > 0)
+                {
+                    lblDaysExecuting.Text = DaysExecuting;
+                    lblDaysExecuting.Draw(g);
+                }
+
             }
         }
 
