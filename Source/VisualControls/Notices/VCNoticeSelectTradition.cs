96304f597e6a3660c65bcc948e5bc36407f22bf1
Максим Кузьмин <cybermax@bk.ru>
2023-03-27 16:52:40 +1000

    + Отображение окна выбора традиции (само окно не реализовано).

--- Source/VisualControls/Notices/VCNoticeSelectTradition.cs
+++ Source/VisualControls/Notices/VCNoticeSelectTradition.cs
@@ -12,6 +12,14 @@ namespace Fantasy_Kingdoms_Battle
         public VCNoticeSelectTradition()
         {
             SetNotice(-1, FormMain.Config.Gui48_Tradition, "Новая традиция", "Выберите традицию", Color.Yellow, false);
+
+            CellEntity.Click += CellEntity_Click;
+        }
+
+        private void CellEntity_Click(object sender, EventArgs e)
+        {
+            WindowSelectTradition w = new WindowSelectTradition();
+            w.Show();
         }
     }
 }
